fcb5f1c78e0376c2924fcd42c1aa717dc79d20e0
Nelson <tugoresn@gmail.com>
2021-07-22 21:26:38 -0300

    descuento/dinamic

--- router/rutasWeb.js
+++ router/rutasWeb.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Ruta Raiz
-router.get('/', (req, res) => {
-  res.render('index');
+router.get("/", (req, res) => {
+  res.render("index");
 });
 
 // Ruta Descuento
-router.get('/descuento', (req, res) => {
+/* router.get('/descuento', (req, res) => {
   res.render('descuento');
-});
+}); */
 
 // Ruta Beneficios
-router.get('/beneficios', (req, res) => {
-  res.render('beneficios');
+router.get("/beneficios", (req, res) => {
+  res.render("beneficios");
 });
 
 // Ruta Comercios
-router.get('/comercios', (req, res) => {
-  res.render('comercios');
+router.get("/comercios", (req, res) => {
+  res.render("comercios");
 });
 
 module.exports = router;
