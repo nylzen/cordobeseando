13f3bb767e280f73e700ea1f5aa13041af67a937
Nylzen <tugoresn@gmail.com>
2021-07-26 16:56:31 -0300

    Filtro por categorias

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -6,41 +6,48 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Configuracion las variables de entorno
-env = require("dotenv").config();
+env = require('dotenv').config();
 
 // Conexion a base de datos
-const user = "nel";
-const pw = "1pJ66SCSiq7jWXtB";
-const dbname = "cordobeseando";
-const mongoose = require("mongoose");
+const user = 'nel';
+const pw = '1pJ66SCSiq7jWXtB';
+const dbname = 'cordobeseando';
+const mongoose = require('mongoose');
 const uri = `mongodb+srv://${user}:${pw}@cluster0.y1fki.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Base conectada"))
+  .then(() => console.log('Base conectada'))
   .catch((e) => console.log(e));
 
 //Motor de plantillas
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 //Configuracion de public
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 // Configuracion de las rutas de la web
-app.use("/", require("./router/rutasWeb"));
-app.use("/clientes", require("./router/clientes"));
-app.use("/descuento", require("./router/descuento"));
-app.use("/beneficios", require("./router/beneficios"));
+app.use('/', require('./router/rutasWeb'));
+app.use('/clientes', require('./router/clientes'));
+app.use('/descuento', require('./router/descuento'));
+app.use('/beneficios', require('./router/beneficios'));
+
+// CATEGORIAS
+app.use('/categorias/animales', require('./router/categorias/animales'));
+app.use('/categorias/arte', require('./router/categorias/arte'));
+app.use('/categorias/automotor', require('./router/categorias/automotor'));
+
+
 
 //Configuracion 404
 app.use((req, res, next) => {
-  res.status(404).render("404", {
-    titulo: "404 not found",
-    descripcion: "Titulo",
+  res.status(404).render('404', {
+    titulo: '404 not found',
+    descripcion: 'Titulo',
   });
 });
 
 app.listen(port, () => {
-  console.log("servidor en el puerto", port);
+  console.log('servidor en el puerto', port);
 });
--- router/rutasWeb.js
+++ router/rutasWeb.js
@@ -6,20 +6,10 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/descuento', (req, res) => {
-  res.render('descuento');
-});
-
 // Ruta Comercios
 router.get('/comercios', (req, res) => {
   res.render('comercios');
 });
 
-// Subir Imagenes a Cloudinary
-router.post('/crear', (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-  res.send('recibido');
-});
 
 module.exports = router;
