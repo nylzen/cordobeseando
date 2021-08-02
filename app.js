ee420c17babacba044d0038ac6a4c2df77c77263
Nylzen <tugoresn@gmail.com>
2021-08-02 16:24:55 -0300

    add comercios dynamic page

--- app.js
+++ app.js
@@ -32,43 +32,68 @@ app.use('/', require('./router/rutasWeb'));
 app.use('/clientes', require('./router/clientes'));
 app.use('/descuento', require('./router/descuento'));
 app.use('/beneficios', require('./router/beneficios'));
+app.use('/comercios', require('./router/comercios'));
 
 // CATEGORIAS
 app.use('/categorias/animales', require('./router/categorias/animales'));
 app.use('/categorias/arte', require('./router/categorias/arte'));
 app.use('/categorias/automotor', require('./router/categorias/automotor'));
 app.use('/categorias/bebes', require('./router/categorias/bebes'));
-// ======= // 
+// ======= //
 app.use('/categorias/belleza', require('./router/categorias/belleza'));
 app.use('/categorias/caza-pesca', require('./router/categorias/caza-pesca'));
 app.use('/categorias/comida', require('./router/categorias/comida'));
 app.use('/categorias/deporte', require('./router/categorias/deporte'));
-// ======= // 
+// ======= //
 app.use('/categorias/electronica', require('./router/categorias/electronica'));
-app.use('/categorias/ferreteria-cerrajeria', require('./router/categorias/ferreteria-cerrajeria'));
+app.use(
+  '/categorias/ferreteria-cerrajeria',
+  require('./router/categorias/ferreteria-cerrajeria')
+);
 app.use('/categorias/heladeria', require('./router/categorias/heladeria'));
 app.use('/categorias/hogar', require('./router/categorias/hogar'));
-// ======= // 
-app.use('/categorias/indumentaria', require('./router/categorias/indumentaria'));
-app.use('/categorias/inmobiliaria', require('./router/categorias/inmobiliaria'));
-app.use('/categorias/joyas-accesorios', require('./router/categorias/joyas-accesorios'));
-app.use('/categorias/kiosco-golosinas', require('./router/categorias/kiosco-golosinas'));
-// ======= // 
+// ======= //
+app.use(
+  '/categorias/indumentaria',
+  require('./router/categorias/indumentaria')
+);
+app.use(
+  '/categorias/inmobiliaria',
+  require('./router/categorias/inmobiliaria')
+);
+app.use(
+  '/categorias/joyas-accesorios',
+  require('./router/categorias/joyas-accesorios')
+);
+app.use(
+  '/categorias/kiosco-golosinas',
+  require('./router/categorias/kiosco-golosinas')
+);
+// ======= //
 app.use('/categorias/librerias', require('./router/categorias/librerias'));
 app.use('/categorias/naturaleza', require('./router/categorias/naturaleza'));
 app.use('/categorias/oficios', require('./router/categorias/oficios'));
 app.use('/categorias/profesiones', require('./router/categorias/profesiones'));
-// ======= // 
-app.use('/categorias/repuestos-accesorios', require('./router/categorias/repuestos-accesorios'));
+// ======= //
+app.use(
+  '/categorias/repuestos-accesorios',
+  require('./router/categorias/repuestos-accesorios')
+);
 app.use('/categorias/rodados', require('./router/categorias/rodados'));
 app.use('/categorias/salud', require('./router/categorias/salud'));
 app.use('/categorias/seguros', require('./router/categorias/seguros'));
-// ======= // 
-app.use('/categorias/souvenir-cotillon', require('./router/categorias/souvenir-cotillon'));
-app.use('/categorias/supermercado', require('./router/categorias/supermercado'));
+// ======= //
+app.use(
+  '/categorias/souvenir-cotillon',
+  require('./router/categorias/souvenir-cotillon')
+);
+app.use(
+  '/categorias/supermercado',
+  require('./router/categorias/supermercado')
+);
 app.use('/categorias/tatuajes', require('./router/categorias/tatuajes'));
 app.use('/categorias/traslados', require('./router/categorias/traslados'));
-// ======= // 
+// ======= //
 app.use('/categorias/videojuegos', require('./router/categorias/videojuegos'));
 
 //Configuracion 404
