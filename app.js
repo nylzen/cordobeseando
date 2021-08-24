00c135e22304b3fd16505065e6e60c9016b3255a
Nylzen <tugoresn@gmail.com>
2021-08-24 18:20:47 -0300

    add obtener-beneficios pasos

--- app.js
+++ app.js
@@ -1,104 +1,104 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Configuracion las variables de entorno
-env = require('dotenv').config();
+env = require("dotenv").config();
 
 // Conexion a base de datos
-const user = 'nel';
-const pw = '1pJ66SCSiq7jWXtB';
-const dbname = 'cordobeseando';
-const mongoose = require('mongoose');
+const user = "nel";
+const pw = "1pJ66SCSiq7jWXtB";
+const dbname = "cordobeseando";
+const mongoose = require("mongoose");
 const uri = `mongodb+srv://${user}:${pw}@cluster0.y1fki.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Base conectada'))
+  .then(() => console.log("Base conectada"))
   .catch((e) => console.log(e));
 
 //Motor de plantillas
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 //Configuracion de public
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // Configuracion de las rutas de la web
-app.use('/', require('./router/rutasWeb'));
-app.use('/clientes', require('./router/clientes'));
-app.use('/descuento', require('./router/descuento'));
-app.use('/beneficios', require('./router/beneficios'));
-app.use('/comercios', require('./router/comercios'));
+app.use("/", require("./router/rutasWeb"));
+app.use("/clientes", require("./router/clientes"));
+app.use("/descuento", require("./router/descuento"));
+app.use("/beneficios", require("./router/beneficios"));
+app.use("/comercios", require("./router/comercios"));
 
 // CATEGORIAS
-app.use('/categorias/animales', require('./router/categorias/animales'));
-app.use('/categorias/arte', require('./router/categorias/arte'));
-app.use('/categorias/bebes', require('./router/categorias/bebes'));
+app.use("/categorias/animales", require("./router/categorias/animales"));
+app.use("/categorias/arte", require("./router/categorias/arte"));
+app.use("/categorias/bebes", require("./router/categorias/bebes"));
 // ======= //
-app.use('/categorias/belleza', require('./router/categorias/belleza'));
-app.use('/categorias/caza-pesca', require('./router/categorias/caza-pesca'));
-app.use('/categorias/comida', require('./router/categorias/comida'));
-app.use('/categorias/deporte', require('./router/categorias/deporte'));
+app.use("/categorias/belleza", require("./router/categorias/belleza"));
+app.use("/categorias/caza-pesca", require("./router/categorias/caza-pesca"));
+app.use("/categorias/comida", require("./router/categorias/comida"));
+app.use("/categorias/deporte", require("./router/categorias/deporte"));
 // ======= //
-app.use('/categorias/electronica', require('./router/categorias/electronica'));
+app.use("/categorias/electronica", require("./router/categorias/electronica"));
 app.use(
-  '/categorias/ferreteria-cerrajeria',
-  require('./router/categorias/ferreteria-cerrajeria')
+  "/categorias/ferreteria-cerrajeria",
+  require("./router/categorias/ferreteria-cerrajeria")
 );
-app.use('/categorias/postres', require('./router/categorias/postres'));
-app.use('/categorias/hogar', require('./router/categorias/hogar'));
+app.use("/categorias/postres", require("./router/categorias/postres"));
+app.use("/categorias/hogar", require("./router/categorias/hogar"));
 // ======= //
 app.use(
-  '/categorias/indumentaria',
-  require('./router/categorias/indumentaria')
+  "/categorias/indumentaria",
+  require("./router/categorias/indumentaria")
 );
 app.use(
-  '/categorias/inmobiliaria',
-  require('./router/categorias/inmobiliaria')
+  "/categorias/inmobiliaria",
+  require("./router/categorias/inmobiliaria")
 );
 app.use(
-  '/categorias/joyas-accesorios',
-  require('./router/categorias/joyas-accesorios')
+  "/categorias/joyas-accesorios",
+  require("./router/categorias/joyas-accesorios")
 );
 app.use(
-  '/categorias/kiosco-golosinas',
-  require('./router/categorias/kiosco-golosinas')
+  "/categorias/kiosco-golosinas",
+  require("./router/categorias/kiosco-golosinas")
 );
 // ======= //
-app.use('/categorias/librerias', require('./router/categorias/librerias'));
-app.use('/categorias/naturaleza', require('./router/categorias/naturaleza'));
-app.use('/categorias/oficios', require('./router/categorias/oficios'));
-app.use('/categorias/profesiones', require('./router/categorias/profesiones'));
+app.use("/categorias/librerias", require("./router/categorias/librerias"));
+app.use("/categorias/naturaleza", require("./router/categorias/naturaleza"));
+app.use("/categorias/oficios", require("./router/categorias/oficios"));
+app.use("/categorias/profesiones", require("./router/categorias/profesiones"));
 // ======= //
 
-app.use('/categorias/rodados', require('./router/categorias/rodados'));
-app.use('/categorias/salud', require('./router/categorias/salud'));
-app.use('/categorias/seguros', require('./router/categorias/seguros'));
+app.use("/categorias/rodados", require("./router/categorias/rodados"));
+app.use("/categorias/salud", require("./router/categorias/salud"));
+app.use("/categorias/seguros", require("./router/categorias/seguros"));
 // ======= //
 app.use(
-  '/categorias/souvenir-cotillon',
-  require('./router/categorias/souvenir-cotillon')
+  "/categorias/souvenir-cotillon",
+  require("./router/categorias/souvenir-cotillon")
 );
 app.use(
-  '/categorias/supermercado',
-  require('./router/categorias/supermercado')
+  "/categorias/supermercado",
+  require("./router/categorias/supermercado")
 );
-app.use('/categorias/tatuajes', require('./router/categorias/tatuajes'));
-app.use('/categorias/traslados', require('./router/categorias/traslados'));
+app.use("/categorias/tatuajes", require("./router/categorias/tatuajes"));
+app.use("/categorias/traslados", require("./router/categorias/traslados"));
 // ======= //
-app.use('/categorias/videojuegos', require('./router/categorias/videojuegos'));
+app.use("/categorias/videojuegos", require("./router/categorias/videojuegos"));
 
 //Configuracion 404
 app.use((req, res, next) => {
-  res.status(404).render('404', {
-    titulo: '404 not found',
-    descripcion: 'Titulo',
+  res.status(404).render("404", {
+    titulo: "404 not found",
+    descripcion: "Titulo",
   });
 });
 
 app.listen(port, () => {
-  console.log('servidor en el puerto', port);
+  console.log("servidor en el puerto", port);
 });
--- router/rutasWeb.js
+++ router/rutasWeb.js
@@ -1,34 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Ruta Raiz
-router.get('/', (req, res) => {
-  res.render('index');
+router.get("/", (req, res) => {
+  res.render("index");
 });
 
 // Ruta Quienes Somos
-router.get('/quienes-somos', (req, res) => {
-  res.render('quienes-somos');
+router.get("/quienes-somos", (req, res) => {
+  res.render("quienes-somos");
 });
 
 // Ruta FAQ
-router.get('/preguntas-frecuentes', (req, res) => {
-  res.render('preguntas-frecuentes');
+router.get("/preguntas-frecuentes", (req, res) => {
+  res.render("preguntas-frecuentes");
 });
 
 // Ruta Pagos
-router.get('/suscripciones', (req, res) => {
-  res.render('suscripciones');
-})
+router.get("/suscripciones", (req, res) => {
+  res.render("suscripciones");
+});
 
 // Ruta FAQ Clientes
-router.get('/preguntas-frecuentes-clientes', (req, res) => {
-  res.render('preguntas-frecuentes-clientes');
-})
+router.get("/preguntas-frecuentes-clientes", (req, res) => {
+  res.render("preguntas-frecuentes-clientes");
+});
 
 // Ruta Pagos
-router.get('/preguntas-frecuentes-suscriptores', (req, res) => {
-  res.render('preguntas-frecuentes-suscriptores');
-})
+router.get("/preguntas-frecuentes-suscriptores", (req, res) => {
+  res.render("preguntas-frecuentes-suscriptores");
+});
 
+// Ruta Obtener beneficios
+router.get("/obtener-beneficios", (req, res) => {
+  res.render("obtener-beneficios");
+});
 module.exports = router;
